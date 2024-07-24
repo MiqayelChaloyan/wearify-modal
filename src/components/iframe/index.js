6d6411f5e070375790bc2baa143c82eec9464ba2
MiqayelChaloyan <miqochaloyan@gmail.com>
2024-07-24 16:15:30 +0400

    fix

--- src/components/iframe/index.js
+++ src/components/iframe/index.js
@@ -8,14 +8,15 @@ const Iframe = ({ src }) => {
         <iframe
             src={src}
             // title="DeepAR Try On"
-            // frameBorder="0"
-            // scrolling="no"
+            frameborder="no"
+                        // scrolling="no"
             className='iframe'
             id='my-iframe'
+            loading="lazy" 
             // allow="fullscreen"
         />
-        {/* <div className='btn_hide'/>
-        <div className='btn_hide_fitting'/> */}
+        <div className='btn_hide'/>
+        <div className='btn_hide_fitting'/>
       </>
     );
 };
