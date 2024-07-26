995d9c65faec3d537b49352489d44fc4a17b3664
MiqayelChaloyan <miqochaloyan@gmail.com>
2024-07-27 02:11:36 +0400

    fix

--- src/components/iframe/index.js
+++ src/components/iframe/index.js
@@ -1,25 +1,73 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
 import './styles.css';
 
-const Iframe = ({ src }) => {
+
+function IFrame({ src, children }) {
+  const [ref, setRef] = useState(null);
+  const container = ref?.contentDocument?.body;
 
   return (
-    <>
-      <iframe
-        src={src}
-        // title="DeepAR Try On"
-        frameborder="0"
-        // scrolling="no"
-        className='iframe'
-        id='my-iframe'
-        loading="lazy"
-      // allow="fullscreen"
-      />
-      {/* <div className='btn_hide'/>
-        <div className='btn_hide_fitting'/> */}
-      {/* <div className='hide-border'/> */}
-    </>
+    <iframe title='iframe' ref={setRef} src={src} className='iframe' frameBorder='0' id='myIframe'>
+      {container && createPortal(children, container)} 
+    </iframe>
   );
-};
+}
+
+const Iframe = ({ src }) => {
+  return (
+    <div className='Iframe'>
+      <div className='hide-border'/>
+      <IFrame src={src} />
+    </div>
+  );
+}
+
 
 export default Iframe;
+
+
+
+
+
+
+
+
+
+
+
+// const Iframe = ({ src }) => {
+
+//   useEffect(() => {
+//     function getIframeContent(frameId) {
+//       let frameObj = 
+//           document.getElementById(frameId);
+//       let frameContent = frameObj.
+//           contentWindow.document.body.innerHTML;
+          
+//       console.log("frame content : " + frameContent);
+//   }
+//   getIframeContent('my-iframe')
+//   }, [src]);
+
+
+//   return (
+//     <>
+//       <iframe
+//         src={src}
+//         // title="DeepAR Try On"
+//         frameBorder="0"
+//         // scrolling="no"
+//         className='iframe'
+//         id='my-iframe'
+//         loading="lazy"
+//       // allow="fullscreen"
+//       />
+//       {/* <div className='btn_hide'/>
+//         <div className='btn_hide_fitting'/> */}
+//       {/* <div className='hide-border'/> */}
+//     </>
+//   );
+// };
+
+// export default Iframe;
