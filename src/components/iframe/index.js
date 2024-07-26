c6dd4f7f1f68a0f8255afdd80c14be49ea6003d7
MiqayelChaloyan <miqochaloyan@gmail.com>
2024-07-26 16:39:53 +0400

    fix

--- src/components/iframe/index.js
+++ src/components/iframe/index.js
@@ -2,24 +2,24 @@ import { useEffect } from 'react';
 import './styles.css';
 
 const Iframe = ({ src }) => {
-    
-    return (
-      <>
-        <iframe
-            src={src}
-            // title="DeepAR Try On"
-            frameborder="no"
-                        // scrolling="no"
-            className='iframe'
-            id='my-iframe'
-            loading="lazy" 
-            // allow="fullscreen"
-        />
-        {/* <div className='btn_hide'/>
+
+  return (
+    <>
+      <iframe
+        src={src}
+        // title="DeepAR Try On"
+        frameborder="0"
+        // scrolling="no"
+        className='iframe'
+        id='my-iframe'
+        loading="lazy"
+      // allow="fullscreen"
+      />
+      {/* <div className='btn_hide'/>
         <div className='btn_hide_fitting'/> */}
-        <div className='hide-border'/>
-      </>
-    );
+      {/* <div className='hide-border'/> */}
+    </>
+  );
 };
 
 export default Iframe;
