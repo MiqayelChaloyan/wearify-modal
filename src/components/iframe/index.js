472b413657ffdaccce799083e588498a01c0df20
MiqayelChaloyan <miqochaloyan@gmail.com>
2024-07-27 03:11:49 +0400

    fix loader

--- src/components/iframe/index.js
+++ src/components/iframe/index.js
@@ -1,73 +1,52 @@
 import { useEffect, useState } from 'react';
-import { createPortal } from 'react-dom';
+
 import './styles.css';
 
 
-function IFrame({ src, children }) {
+function IFrame({ src }) {
   const [ref, setRef] = useState(null);
-  const container = ref?.contentDocument?.body;
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    if (ref) {
+      const handleLoad = () => {
+        setLoaded(true);
+      };
+      ref.addEventListener('load', handleLoad);
+      return () => {
+        ref.removeEventListener('load', handleLoad);
+      };
+    }
+  }, [ref]);
+
 
   return (
-    <iframe title='iframe' ref={setRef} src={src} className='iframe' frameBorder='0' id='myIframe'>
-      {container && createPortal(children, container)} 
-    </iframe>
+    <>
+      {!loaded && <div className="loader">Loading...</div>}
+      <iframe
+        title="iframe"
+        ref={setRef}
+        src={src}
+        className={`iframe ${loaded ? 'loaded' : ''}`}
+        frameBorder="0"
+        id="myIframe"
+        sandbox={true}
+        loading="eager"
+      />
+    </>
   );
-}
+};
+
 
 const Iframe = ({ src }) => {
   return (
-    <div className='Iframe'>
-      <div className='hide-border'/>
+    <div className="Iframe">
+      <div className="hide-border" />
       <IFrame src={src} />
     </div>
   );
 }
 
-
 export default Iframe;
 
 
-
-
-
-
-
-
-
-
-
-// const Iframe = ({ src }) => {
-
-//   useEffect(() => {
-//     function getIframeContent(frameId) {
-//       let frameObj = 
-//           document.getElementById(frameId);
-//       let frameContent = frameObj.
-//           contentWindow.document.body.innerHTML;
-          
-//       console.log("frame content : " + frameContent);
-//   }
-//   getIframeContent('my-iframe')
-//   }, [src]);
-
-
-//   return (
-//     <>
-//       <iframe
-//         src={src}
-//         // title="DeepAR Try On"
-//         frameBorder="0"
-//         // scrolling="no"
-//         className='iframe'
-//         id='my-iframe'
-//         loading="lazy"
-//       // allow="fullscreen"
-//       />
-//       {/* <div className='btn_hide'/>
-//         <div className='btn_hide_fitting'/> */}
-//       {/* <div className='hide-border'/> */}
-//     </>
-//   );
-// };
-
-// export default Iframe;
